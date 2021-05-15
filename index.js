444d8b4eb5aeb37c7c4df2061dbd61a0c01dbf97
maxrabin <max.rabin17@gmail.com>
2021-05-15 11:55:22 -0400

    commit tree 92238440744c7acd286633326f610cdef941d1e2

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+function createEmployeeRecord(array) {
+    let record;
+    return record = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(){
+    return array.map(createEmployeeRecord)
+}
+
+function createDSObj(getType, dateStamp) {
+    return {type: getType, date: dateStamp.slice(0,10), hour: parseInt(dateStamp.slice(-4))}
+}
